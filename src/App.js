101b6e00fe9d9e54ea739e31bb6cd2597dbcd3bf
Jade <jade-westwood@outlook.com>
2023-11-17 11:05:14 +0000

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather app</div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <h1>Weather app</h1>
+      </div>
+    </div>
+  );
 }
 
 export default App;
